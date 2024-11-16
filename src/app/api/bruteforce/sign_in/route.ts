cf0f6136127147670c17c278fdcebf5ec8167393
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-11-16 17:04:45 +0200

    move rateLimiter to bruteforce folder

--- src/app/api/bruteforce/sign_in/route.ts
+++ src/app/api/bruteforce/sign_in/route.ts
@@ -3,7 +3,7 @@ import { db } from "@/lib/db";
 import { user as userTable } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 import * as argon2 from "argon2";
-import rateLimitMiddleware from "../../rateLimiter";
+import rateLimitMiddleware from "../rateLimiter";
 
 async function signIn(request: Request) {
     const body = await request.json();
