1db7e5b9c8b47c336c526ef0f1f06ab2946725d2
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-11-16 22:39:05 +0200

    update schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "drizzle-kit";
 
 export default {
-    schema: "./src/server/db/schema.ts",
+    schema: "./src/lib/db/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
         url: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:${process.env.DATABASE_PORT}/${process.env.DATABASE_DB}`,
